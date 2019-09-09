df441b258fcb4e71818739275dece78963f5cef4
Noah Newkirk <nsnewkir@iu.edu>
2019-09-09 13:24:03 -0400

    In-Class 9/9

---
+++ app/public/dummy.php
@@ -0,0 +1,21 @@
+{
+   "patient": [
+
+      {
+         "PatientGuid": "01",
+         "firstName": "Alex",
+         "lastName": "Lile",
+         "dob": "1996-05-10",
+         "sexAtBirth":"M"
+      }
+
+      {
+         "PatientGuid": "02",
+         "firstName": "Josh",
+         "lastName": "Mawson",
+         "dob": "1996-03-20",
+         "sexAtBirth":"M"
+      }
+
+   ]
+}
