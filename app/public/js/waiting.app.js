3dc38459d0b273d762e1e450222450ef726b01d7
Noah Newkirk <nsnewkir@iu.edu>
2019-09-13 18:05:17 -0400

    omg i actually got it to work

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -1,45 +1,23 @@
 var waitingApp = new Vue({
   el: '#patientWaitingApp',
   data: {
-    patients:  [
+    person:  [
     {
-       "PatientGuid": "01",
-       "firstName": "Alex",
-       "lastName": "Lile",
-       "dob": "1996-05-10",
-       "sexAtBirth":"M",
-       priority: "high"
-    },
-    {
-       "PatientGuid": "02",
-       "firstName": "Josh",
-       "lastName": "Mawson",
-       "dob": "1996-03-20",
-       "sexAtBirth":"M",
-       priority: "medium"
+
     }
-    {
-       "PatientGuid": "03",
-       "firstName": "Autumn",
-       "lastName": "Triscuit",
-       "dob": "1996-07-27",
-       "sexAtBirth":"F",
-       priority: "low"
-     }
   ]
 },
   methods: {
       fetchPatients() {
-          //fetch('https://randomuser.me/api/')
-          fetch('dummy.php')
+          fetch('https://randomuser.me/api/')
           .then( response => response.json() )
-          .then( json => {waitingApp.patients = json})
+          .then( json => {waitingApp.person = json.results[0]})
           //.then( function(result) {
             //return result.json()
           //} )
           //.catch(/*function goes here*/)
           ;
-          thenDoThis();
+          //thenDoThis();
       }
   },
   created() {
